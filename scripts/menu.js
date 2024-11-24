878c12701d1ad8601e0c5721b2b5b052e964ad85
Gabriel Mello <gabrielpdmello@gmail.com>
2024-11-24 20:34:57 -0300

    Rename const variable for the open/close menu animation

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,15 @@
 const navButton = document.querySelector(".nav__button");
 const navUl = document.querySelector(".nav__ul");
-const test1 = document.querySelector(".svg-close-animation");
-const test2 = document.querySelector(".svg-open-animation");
+const closeAnimation = document.querySelector(".svg-close-animation");
+const openAnimation = document.querySelector(".svg-open-animation");
 const body = document.body;
 
 document.addEventListener("click", e => {
     if (navButton.contains(e.target) && !navUl.classList.contains("show")) {
         navUl.classList.add("show");
-        test2.beginElement();
+        openAnimation.beginElement();
     } else if (!navUl.contains(e.target) && navUl.classList.contains("show")) {
         navUl.classList.remove("show");
-        test1.beginElement();
+        closeAnimation.beginElement();
     }
 });
